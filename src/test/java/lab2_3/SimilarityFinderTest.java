8831775a25bf7a90f43d8fd47a8428c584de604c
Sebastian Piecowski <seba.piecowski@gmail.com>
2018-03-23 13:27:56 +0100

    Dodane CalculateJackardSimilarityForEmptySequencesTest dla pustych sekwencji

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -13,11 +13,17 @@ public class SimilarityFinderTest {
 	public void CalculateJackardSimilarityTest() {
 		SimilarityFinder similarityFinder=new SimilarityFinder(searcher);
 		int[] seq1= {1,2,3,4,5};
-		int[] seq2= {1,2,3,4,5,6,7,8,9};
+		int[] seq2= {1,2,3,4,5,6,7,8,9,10};
 		int intersect=5;
 		int union=seq1.length+seq2.length-intersect;
 		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is((double)intersect/(double)union));
-		//should return 
+		//should return 2.0
 	}
-
+	@Test
+	public void CalculateJackardSimilarityForEmptySequencesTest() {
+		SimilarityFinder similarityFinder=new SimilarityFinder(searcher);
+		int[] seq1= {};
+		int[] seq2= {};
+		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is(1.0d));
+	}
 }
