fa11345f9c315e6d0a37cc011bc6328de9cbec37
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-25 19:48:19 +0200

    Dodanie klasy MockSequenceSearcher

--- src/test/java/lab2_3/SimilarityFinderTests.java
+++ src/test/java/lab2_3/SimilarityFinderTests.java
@@ -3,7 +3,6 @@ package lab2_3;
 import static org.junit.Assert.*;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.powermock.api.mockito.PowerMockito.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,32 +10,11 @@ import org.junit.Test;
 import edu.iis.mto.similarity.SimilarityFinder;
 
 public class SimilarityFinderTests {
-	
-	private static SimilarityFinder similarityFinder;
-    private static SequenceSearcher sequenceSearcher;
-    private static SearchResult searchResult;
-	
-    private static int[] sequenceA;
-    private static int[] sequenceB;
-    
-	@BeforeClass
-	public void setUp(){
-		sequenceSearcher = new SequenceSearcher() {
-			public SearchResult search(int i, int[] sequence) {
-                for (int k : sequence) {
-                    if(k == i) 
-                    	return searchResult;
-                }
-                return Mockito.mock(SearchResult.class);
-		}
-		
-	}
+
 
 	@Test
 	public void ShouldReturnOneIfBothSetsAreEmpty() {
-		int[] setA = new int[0];
-		int[] setB = new int[0];
-		assertThat();
+		
 	}
 
 }
