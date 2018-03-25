f7a210fe6fbde97e3dd236a1d9df94d0d84c8829
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-25 19:52:29 +0200

    Dodanie testu CalculateJackardSimilarityOneSequenceIsEmpty

--- src/test/java/lab2_3/SimilarityFinderTests.java
+++ src/test/java/lab2_3/SimilarityFinderTests.java
@@ -7,14 +7,21 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.iis.mto.similarity.MockSequenceSearcher;
 import edu.iis.mto.similarity.SimilarityFinder;
 
 public class SimilarityFinderTests {
 
 
+SimilarityFinder finder = new SimilarityFinder(new MockSequenceSearcher());
+	
 	@Test
-	public void ShouldReturnOneIfBothSetsAreEmpty() {
+	public void CalculateJackardSimilarityOneSequenceIsEmpty() {
 		
+		int[] seqA = {};
+		int[] seqB = {2, 4, 5, 7, 8};
+		final double EXPECTED = finder.calculateJackardSimilarity(seqA, seqB);			
+		assertThat(EXPECTED, is(0d));
 	}
 
 }
