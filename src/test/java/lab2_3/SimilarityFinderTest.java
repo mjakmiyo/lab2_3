ca7566ae9545cd878096ca2c6747151c486f4f5f
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-03-26 17:32:36 +0200

    Test dla sekwencji z wspolnymi elementami

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -57,10 +57,17 @@ public class SimilarityFinderTest {
 		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is(1.0d));
 	}
 	@Test
-	public void CaluculateJackardSimilatiryForTwoDistinctSequencesTest() {
+	public void CaluculateJackardSimilarityForTwoDistinctSequencesTest() {
 		int[] seq1= {1,2,3,4};
 		int[] seq2= {5,6,7,8,9};
 		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is(0d));
 	}
+	@Test
+	public void CalculateJackardSimilarityForCommonElementsInSequencesTest() {
+		int[] seq1= {1,2,3,4};
+		int[] seq2= {3,4,5,6};
+		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is(2/6d));
+	}
+	
 	
 }
