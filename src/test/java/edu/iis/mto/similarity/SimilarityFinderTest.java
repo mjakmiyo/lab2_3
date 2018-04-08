15f6feb330c8917bccd65c51470b129f806a21dc
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-04-08 17:23:30 +0200

    Refactor test expected from formula to double value.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -18,56 +18,56 @@ public class SimilarityFinderTest {
     public void twoEmptySequencesTest() throws Exception {
         int[] seq1 = {};
         int[] seq2 = {};
-        double intersection = 1;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection ) );
+        double expectedResult = 1;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void firstEmptySequenceTest() throws Exception {
         int[] seq1 = {};
         int[] seq2 = {2, 3, 4};
-        double intersection = 0;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection ) );
+        double expectedResult = 0;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void secondEmptySequenceTest() throws Exception {
         int[] seq1 = {3, 4, 5};
         int[] seq2 = {};
-        double intersection = 0;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection ) );
+        double expectedResult = 0;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void notEmptySequencesWithoutIntersectionTest() throws Exception {
         int[] seq1 = {3, 4, 5};
         int[] seq2 = {6, 7, 8};
-        double intersection = 0;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection ) );
+        double expectedResult = 0;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void notEmptySequencesWithOneIntersectionTest() throws Exception {
         int[] seq1 = {3, 4, 5, 6};
         int[] seq2 = {6, 7, 8};
-        double intersection = 1;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection / (seq1.length + seq2.length - intersection) ) );
+        double expectedResult = 1 / 6d;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void notEmptySequencesWithManyIntersectionTest() throws Exception {
         int[] seq1 = {3, 4, 5, 6};
         int[] seq2 = {4, 5, 6, 7, 8};
-        double intersection = 3;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection / (seq1.length + seq2.length - intersection) ) );
+        double expectedResult = 3 / 6d;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
     @Test
     public void firstSequenceInSecondSequenceTest() throws Exception {
         int[] seq1 = {1, 2, 3, 4};
         int[] seq2 = {1, 2, 3, 4, 5, 6, 7, 8};
-        double intersection = 4;
-        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( intersection / (seq1.length + seq2.length - intersection) ) );
+        double expectedResult = 4 / 8d;
+        assertThat( similarityFinder.calculateJackardSimilarity( seq1, seq2 ), is( expectedResult ) );
     }
 
 }
