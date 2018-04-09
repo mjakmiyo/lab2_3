884b4ea4bb089c96930ae667a50492e6946de107
Schocker <adikorzeniowski@gmail.com>
2018-04-09 12:31:43 +0200

    Add first test with two empty sequences for SimilarityFinder

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,27 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+    private SimilarityFinder similarityFinder;
+    private SequenceSearcher sequenceSearcher;
+
+    @Before
+    public void setUp() {
+        sequenceSearcher = new SequenceSearcherStub();
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+    }
+
+    @Test
+    public void twoEmptySequencesShouldReturn1() {
+        int[] sequence1 = {};
+        int[] sequence2 = {};
+
+        assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(1d));
+    }
+}
