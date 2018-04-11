e0bac9142e635eacaf59e3722ecf95d648390a70
Cezary Witkowski <czarecoo@gmail.com>
2018-04-11 14:59:54 +0200

    Deleted unnecessary methods in SearchResultStub class

--- src/test/java/edu/iis/mto/similarity/SearchResultStub.java
+++ src/test/java/edu/iis/mto/similarity/SearchResultStub.java
@@ -3,22 +3,19 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 
 public class SearchResultStub implements SearchResult {
+
     private boolean isFound;
     private int position;
 
-    public SearchResultStub(boolean isFound){
-    	this.isFound = isFound;
-    }
-    public boolean isFound(){
-        return isFound;
-    }
-    public void setFound(boolean isFound){
+    public SearchResultStub(boolean isFound) {
         this.isFound = isFound;
     }
-    public int getPosition(){
+
+    public boolean isFound() {
+        return isFound;
+    }
+
+    public int getPosition() {
         return position;
     }
-    public void setPosition(int position){
-        this.position = position;
-}
 }
