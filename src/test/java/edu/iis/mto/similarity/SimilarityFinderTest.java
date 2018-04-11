93928298b1ae074b39b347a05a33c887685c46f7
Cezary Witkowski <czarecoo@gmail.com>
2018-04-11 14:40:12 +0200

    Added another test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,20 +1,34 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import edu.iis.mto.search.SequenceSearcher;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+import edu.iis.mto.search.SequenceSearcher;
+
 public class SimilarityFinderTest {
-	private SimilarityFinder similarityFinder;
-	 @Test
-	    public void calculateJackardSimilarityShouldReturnCorrectValue(){
-	        int[] sequence1 = {1,2,3,4};
-	        int[] sequence2 = {1,2,3,5};
-	        SequenceSearcher sequenceSearcher = new SequenceSearcherStub(new boolean[]{true, true, true, false});
-	        similarityFinder = new SimilarityFinder(sequenceSearcher);
-	        double result = similarityFinder.calculateJackardSimilarity(sequence1, sequence2);
-	        assertThat(result, is(3.0/5.0));
-	 }
+
+    private SimilarityFinder similarityFinder;
+
+    @Test
+    public void calculateJackardSimilarityShouldReturnCorrectValue() {
+        int[] sequence1 = {1, 2, 3, 4};
+        int[] sequence2 = {1, 2, 3, 5};
+        SequenceSearcher sequenceSearcher = new SequenceSearcherStub(new boolean[] {true, true, true, false});
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        double result = similarityFinder.calculateJackardSimilarity(sequence1, sequence2);
+        assertThat(result, is(3.0 / 5.0));
+    }
+
+    @Test
+    public void calculateJackardSimilarityShouldReturnOneForIdenticalSequences() {
+        int[] sequence1 = {1, 2, 3, 4, 5, 6};
+        int[] sequence2 = {1, 2, 3, 4, 5, 6};
+        SequenceSearcher sequenceSearcher = new SequenceSearcherStub(
+                new boolean[] {true, true, true, true, true, true});
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        double result = similarityFinder.calculateJackardSimilarity(sequence1, sequence2);
+        assertThat(result, is(1.0));
+    }
 }
