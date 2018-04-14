e7ac00db8d77d54c38e1d07120ebc5a11952ae48
Marcin Kapelan <marcinkape@gmail.com>
2018-04-14 12:54:56 +0200

    Add tests for non empty sequences

--- src/test/java/SimilarityFinderTests.java
+++ src/test/java/SimilarityFinderTests.java
@@ -37,4 +37,34 @@ public class SimilarityFinderTests {
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherStub);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(0.0));
     }
+
+    @Test
+    public void calculatingJackardSimilarityForTwoSameSequencesShouldReturnOne() {
+        boolean[] expectedResults = {true, true, true};
+        int[] sequence1 = {2, 4, 6};
+        int[] sequence2 = {2, 4, 6};
+        SequenceSearcher sequenceSearcherStub = new SequenceSearcherStub(expectedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherStub);
+        assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(1.0));
+    }
+
+    @Test
+    public void calculatingJackardSimilarityForTwoSequencesWithSameValuesShouldReturnOne() {
+        boolean[] expectedResults = {true, true, true};
+        int[] sequence1 = {6, 2, 4};
+        int[] sequence2 = {2, 4, 6};
+        SequenceSearcher sequenceSearcherStub = new SequenceSearcherStub(expectedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherStub);
+        assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(1.0));
+    }
+
+    @Test
+    public void calculatingJackardSimilarityForTwoDifferentSequencesShouldReturnCorrectValue() {
+        boolean[] expectedResults = {false, true, true, false};
+        int[] sequence1 = {0, 5, 7, 9};
+        int[] sequence2 = {5, 7, 1, 517, 2};
+        SequenceSearcher sequenceSearcherStub = new SequenceSearcherStub(expectedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherStub);
+        assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(2.0 / 7.0));
+    }
 }
