fbb15ff7143a2e73cbe64d31dbfa50e83a5508a5
gronek.grzegorz <202154@edu.p.lodz.pl>
2018-04-17 10:23:12 +0200

    changed the conception of the tests; added tests for empty sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,30 +1,34 @@
 package edu.iis.mto.similarity;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.Matchers;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.iis.mto.mock.MockSimilarityFinder;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SimilarityFinderTest {
 
-    static SequenceSearcher search;
-    public static int union;
-    public static SimilarityFinder similarityFinder;
-    public static int[] seq1 = {1, 2, 3, 4, 5};
-    public static int[] seq2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    public static int intersect = 5;
-
-    @BeforeClass
-    public static void setUp() {
-        similarityFinder = new SimilarityFinder(search);
-        union = seq1.length + seq2.length - intersect;
+    @Test
+    public void testingTwoEmptySequencesShouldReturnOne() {
+        boolean expectedResults[] = {false};
+        SequenceSearcher sequenceSearcher = new MockSimilarityFinder(expectedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+        int seq1[] = {};
+        int seq2[] = {};
+        double expectedResult = 1.0;
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(expectedResult));
     }
 
     @Test
-    public void CalculateJackardSimilarityTest() {
-        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is((double) intersect / union));
+    public void testingOneEmptySequenceShouldReturnZero() {
+        boolean expectedResults[] = {false, false};
+        SequenceSearcher sequenceSearcher = new MockSimilarityFinder(expectedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+        int seq1[] = {1, 2};
+        int seq2[] = {};
+        double expectedResult = 0;
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(expectedResult));
     }
 }
