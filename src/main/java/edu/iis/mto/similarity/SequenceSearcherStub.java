730b53977f67ae759a2685ba26b90cfc0a3be9d4
Schocker <adikorzeniowski@gmail.com>
2018-04-23 19:37:51 +0200

    Get rid of search algorithm in SequenceSearcherStub

--- src/main/java/edu/iis/mto/similarity/SequenceSearcherStub.java
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherStub.java
@@ -4,10 +4,17 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherStub implements SequenceSearcher {
+    private boolean[] resultsArray;
+    private int currentElement;
+
+    public void setResultsArray(boolean[] resultsArray) {
+        this.resultsArray = resultsArray;
+        currentElement = 0;
+    }
+
     public SearchResult search(int needle, int[] hay) {
-        for (int i : hay) {
-            if (i == needle) return new SearchResultStub(true, i);
-        }
-        return new SearchResultStub(false, -1);
+        SearchResultStub searchResultStub =  new SearchResultStub(resultsArray[currentElement], currentElement);
+        currentElement++;
+        return searchResultStub;
     }
 }
--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -9,10 +9,11 @@ import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
     private SimilarityFinder similarityFinder;
+    private SequenceSearcherStub sequenceSearcher;
 
     @Before
     public void setUp() {
-        SequenceSearcher sequenceSearcher = new SequenceSearcherStub();
+        sequenceSearcher = new SequenceSearcherStub();
         similarityFinder = new SimilarityFinder(sequenceSearcher);
     }
 
@@ -26,9 +27,11 @@ public class SimilarityFinderTest {
 
     @Test
     public void twoSameSequencesShouldReturn1() {
+        boolean[] expectedResults = {true, true, true};
         int[] sequence1 = {1, 2, 3};
         int[] sequence2 = {1, 2, 3};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(1d));
     }
 
@@ -42,41 +45,51 @@ public class SimilarityFinderTest {
 
     @Test
     public void onlySecondSequenceEmptyShouldReturn0() {
+        boolean[] expectedResults = {false, false, false};
         int[] sequence1 = {4, 5, 6};
         int[] sequence2 = {};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(0d));
     }
 
     @Test
     public void twoDifferentSequencesShouldReturn0() {
+        boolean[] expectedResults = {false, false, false};
         int[] sequence1 = {1, 2, 3};
         int[] sequence2 = {4, 5, 6};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(0d));
     }
 
     @Test
     public void twoSequencesWithOneOverlapShouldReturnCorrectValue() {
+        boolean[] expectedResults = {true, false, false};
         int[] sequence1 = {7, 8, 9};
         int[] sequence2 = {5, 6, 7};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(1 / 5d));
     }
 
     @Test
     public void twoSequencesWithManyOverlapsShouldReturnCorrectValue() {
+        boolean[] expectedResults = {false, true, false, true, false};
         int[] sequence1 = {1, 2, 3, 4, 5};
         int[] sequence2 = {2, 4, 6, 8};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(2 / 7d));
     }
 
     @Test
     public void twoSequencesWithOneCompletelyOverlappingShouldReturnCorrectValue() {
+        boolean[] expectedResults = {true, true, true, false, false};
         int[] sequence1 = {1, 2, 3, 4, 5};
         int[] sequence2 = {1, 2, 3};
 
+        sequenceSearcher.setResultsArray(expectedResults);
         assertThat(similarityFinder.calculateJackardSimilarity(sequence1, sequence2), is(3 / 5d));
     }
 }
