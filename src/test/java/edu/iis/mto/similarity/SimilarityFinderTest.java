a0e9f0184e638190fca0ed84c29990752937a79a
rogo6 <piotrrogozinski95@gmail.com>
2018-05-01 17:37:43 +0200

    Added tests for similarityFinder

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,68 @@
+package edu.iis.mto.similarity;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import edu.iis.mto.search.SequenceSearcher;
+
+import static org.hamcrest.Matchers.is;
+
+
+public class SimilarityFinderTest {
+	
+	private SimilarityFinder similarityFinder;
+	
+	@Test
+	public void calculateJackardSimilarityShouldReturnOneForEmptySequences(){
+		
+		boolean [] results= {};
+		int [] firstSequence= {};
+		int [] secondSequence= {};
+		
+		SequenceSearcher sequenceSearcherStub= new SequenceSearcherStub(results);
+		similarityFinder= new SimilarityFinder(sequenceSearcherStub);
+		double expectedValue=similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence);
+		
+		assertThat(expectedValue,is(1.0));
+	}
+	
+	@Test
+	public void calcuateJackardSimilarityShouldReturnZeroWhenFirstSequenceIsEmpty(){
+		boolean [] results= {};
+		int [] firstSequence= {};
+		int [] secondSequence= {5,3,2};
+		
+		SequenceSearcher sequenceSearcherStub= new SequenceSearcherStub(results);
+		similarityFinder= new SimilarityFinder(sequenceSearcherStub);
+		double expectedValue=similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence);
+		
+		assertThat(expectedValue,is(0.0));
+	}
+	
+	@Test
+	public void calcuateJackardSimilarityShouldReturnZeroWhenSecondSequenceIsEmpty(){
+		boolean [] results= {};
+		int [] firstSequence= {123,11,0,2};
+		int [] secondSequence= {};
+		
+		SequenceSearcher sequenceSearcherStub= new SequenceSearcherStub(results);
+		similarityFinder= new SimilarityFinder(sequenceSearcherStub);
+		double expectedValue=similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence);
+		
+		assertThat(expectedValue,is(0.0));
+	}
+	
+	@Test
+	public void calculateJackardSimilarityShouldReturnCorrectResultWhenSequencesArentEmpty() {
+		boolean [] results= {false,false,true,true};
+		int [] firstSequence= {2,4,5,6};
+		int [] secondSequence= {5,6,7,8};
+		SequenceSearcher sequenceSearcherStub= new SequenceSearcherStub(results);
+		similarityFinder= new SimilarityFinder(sequenceSearcherStub);
+		double expectedValue=similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence);
+		
+		assertThat(expectedValue,is(2.0/6.0));
+		
+	}
+}
