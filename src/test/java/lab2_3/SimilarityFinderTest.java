9834c9b6b5c1d6ca435f8354847e8796309cd425
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-09-05 05:10:28 +0200

    Added doubler and CalculateJackardSimilarityForEmptySequencesTest test

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -1,23 +1,49 @@
 package lab2_3;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 import edu.iis.mto.similarity.SimilarityFinder;
 public class SimilarityFinderTest {
 	private static SimilarityFinder similarityFinder;
 	private static SequenceSearcher searcher;
+	private static SearchResult searchResult;
 	@BeforeClass
 	public static void init() {
+		searchResult = mock(SearchResult.class);
+		searcher = new SequenceSearcher() {
+			
+			public SearchResult search(int key, int[] seq) {
+				for (int i: seq) {
+					if(i==key) 
+						return searchResult;
+				}
+				return mock(SearchResult.class);
+			}
+		};
 		similarityFinder=new SimilarityFinder(searcher);
 	}
+
+
+	@Before
+ 	public void initMockMethod() {
+ 		initMocks(this);
+ 		when(searchResult.isFound()).thenReturn(true);
+ 	}
+	
 	@Test
 	public void CalculateJackardSimilarityForEmptySequencesTest() {
 		int[] seq1= {};
 		int[] seq2= {};
 		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.is(1.0d));
 	}
+	
 }
