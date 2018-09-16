115c32bf4fd9e309256877af9700592df9a2ee95
mjakmiyo <jedrzejekmonika@gmail.com>
2018-09-16 17:40:09 +0200

    Add SequenceSearcherPart and SearchResultPart classes

---
+++ src/main/java/edu/iis/mto/similarity/SearchResultPart.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+
+public class SearchResultPart implements SearchResult {
+	public int getPosition() {
+		return 0;
+	}
+
+	public boolean isFound() {
+		return false;
+	}
+}
---
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherPart.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.similarity;
+
+import javax.naming.directory.SearchResult;
+
+import edu.iis.mto.search.SequenceSearcher;
+
+public class SequenceSearcherPart implements SequenceSearcher {
+	public SearchResult search(int i, int[] ints) {
+		return null;
+	}
+}
